6bd8976179150ea949d2bf1c3fad15f42e057e06
Adrian <adrian.kizlauskas@brickblock.io>
2017-11-12 04:39:23 +0100

    minimal Mintable AccessToken

--- contracts/BrickblockAccessToken.sol
+++ contracts/BrickblockAccessToken.sol
@@ -1,10 +1,20 @@
 pragma solidity ^0.4.4;
 
-import 'zeppelin-solidity/contracts/token/BurnableToken.sol';
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
-contract BrickblockAccessToken is BurnableToken {
-  string public test;
-  function BurnableToken(string _test) {
-    test = _test;
+contract BrickblockAccessToken is StandardToken {
+  event Mint(address indexed to, uint256 amount);
+
+  function BrickblockAccessToken() {
+    
   }
+
+  function mint(address _to, uint256 _amount)  public returns (bool) {
+    totalSupply = totalSupply.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    Transfer(0x0, _to, _amount);
+    return true;
+  }
+
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
-const BrickblockToken = artifacts.require("./BrickblockToken.sol")
-const Brickblock = artifacts.require("./Brickblock.sol")
-const BrickblockFountain = artifacts.require("./BrickblockFountain.sol");
+const BrickblockToken = artifacts.require('./BrickblockToken.sol')
+const BrickblockFountain = artifacts.require('./BrickblockFountain.sol');
+const BrickblockAccessToken = artifacts.require('./BrickblockAccessToken.sol');
+const Brickblock = artifacts.require('./Brickblock.sol')
 
 module.exports = deployer => {
   return deployer.deploy(BrickblockToken)
-    .then( () => deployer.deploy(BrickblockFountain, BrickblockToken.address) )
+    .then( () => deployer.deploy(BrickblockAccessToken) )
+    .then( () => deployer.deploy(BrickblockFountain, BrickblockToken.address, BrickblockAccessToken.address) )
     .then( () => deployer.deploy(Brickblock) )
 
 };
