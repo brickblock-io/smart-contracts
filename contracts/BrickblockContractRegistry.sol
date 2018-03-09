2b6ca615a56fff702268c92365a481bc76f122a4
mattgstevens <mattgstevens@brickblock.io>
2018-03-09 16:02:54 +0000

    Chore/event suffix

--- contracts/BrickblockContractRegistry.sol
+++ contracts/BrickblockContractRegistry.sol
@@ -8,12 +8,15 @@ contract BrickblockContractRegistry is Ownable {
   address public owner;
   mapping (bytes => address) contractAddresses;
 
+  event UpdateContractEvent(string name, address indexed contractAddress);
+
   function updateContract(string _name, address _address)
     public
     onlyOwner
     returns (address)
   {
     contractAddresses[bytes(_name)] = _address;
+    UpdateContractEvent(_name, _address);
   }
 
   function getContractAddress(string _name)
