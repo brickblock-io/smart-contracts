c24ff2c87f860f2f20fbd68a242c7b2b0cf3829b
Volkan Bilici <volkan.bilici@brickblock.io>
2018-08-29 12:15:02 +0300

    improve deployment help

--- migrations/helpers/arguments.js
+++ migrations/helpers/arguments.js
@@ -1,33 +1,43 @@
 const yargs = require('yargs')
   .option('all', {
     alias: 'a',
-    describe: 'Deploys eco-system with all options'
+    describe: 'Deploys eco-system with all options',
+    default: false
   })
   .option('register', {
     alias: 'r',
-    describe: 'Registers contracts to contract registry'
+    describe: 'Registers contracts to contract registry',
+    default: false
   })
   .option('setRate', {
     alias: 'sr',
-    describe: 'Sets currency rate'
+    describe: 'Sets currency rate',
+    default: false
   })
   .option('finalizeBbk', {
     alias: 'fb',
-    describe: 'finalizes crowdsale, distributes bbk if not mainnet'
+    describe: 'finalizes crowdsale, distributes bbk if not mainnet',
+    default: false
   })
   .option('addBroker', {
     alias: 'ab',
-    describe: 'Adds broker to PoaManager'
+    describe: 'Adds broker to PoaManager',
+    default: false
   })
   .option('deployPoa', {
     alias: 'dp',
-    describe: 'Adds broker to PoaManager'
+    describe: 'deploys an example POA token',
+    default: false
   })
   .option('addToWhiteList', {
-    alias: 'aw'
+    alias: 'aw',
+    describe: 'adds accounts to whitelist',
+    default: false
   })
   .option('useExistingContracts', {
     alias: 'uec',
+    describe:
+      'Uses existing contracts instead of a new deploy if they exist in "config/deployed-contracts.js"',
     default: false
   })
   .help()
